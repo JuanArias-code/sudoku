df090660fbab40420ad66f52a98823c3d258d0df
tphanco <phan.d.tt@gmail.com>
2015-11-12 17:24:18 -0600

    Add solution print out for solve_dfs

--- DFS_Sudoku.py
+++ DFS_Sudoku.py
@@ -120,13 +120,14 @@ def depth_first_search(problem):
 def solve_dfs(board):
     print ("\nSolving with DFS...")
     start_time = time.time()
-    problem = Problem(board)
-    solution = depth_first_search(problem)
+    solution = backtracking(board, 0)
     elapsed_time = time.time() - start_time
 
-    if solution[0]:
+    if solution:
         print ("Found solution")
+        for row in solution:
+            print (row)
     else:
         print ("No possible solutions")
 
-    print ("Elapsed time: " + str(elapsed_time))
+    print ("\nElapsed time: " + str(elapsed_time))
